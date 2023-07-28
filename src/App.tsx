0aa91b388030d68f67e3c33b631e90069d088743
Shawn Lee <thisisshawnlee@gmail.com>
2023-07-28 13:16:44 +0800

    edit landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
+import heroImage from './assets/robothand.png'
+
 function App() {
     return (
         <main className="min-h-screen min-w-screen dark:text-white dark:bg-zinc-900">
-            <h1 className="pt-6 text-4xl font-bold lg:text-6xl">
-                I implement machine learning technologies
-            </h1>
+            <div className="pt-16 lg:flex lg:place-items-center lg:justify-between">
+                <div className="px-4 lg:pl-32">
+                    <h1 className="text-4xl font-bold tracking-tight sm:text-6xl">
+                        I build things with machine learning
+                    </h1>
+                    <p className="mt-6 text-slate-600 dark:text-slate-400">
+                        Hello, my name is Shawn! I like experimenting with machine learning algorithms.
+                        Currently, I study Data Science at Ngee Ann Polytechnic.
+                    </p>
+                </div>
+                <div className="max-w-screen-lg">
+                    <img src={heroImage} alt="heroimage" className='relative right-0'/>
+                </div>
+            </div>
         </main>
     )
 }
