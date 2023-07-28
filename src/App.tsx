b23afdea079c453ef3f8d0632349f6cbf7c7d7a5
Shawn Lee <thisisshawnlee@gmail.com>
2023-07-28 12:34:32 +0800

    edit indentation to 4 spaces

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 function App() {
-  return (
-    <main className="min-h-screen min-w-screen dark:text-white dark:bg-zinc-900">
-      <h1 className="pt-6 text-4xl font-bold lg:text-6xl">
-        I implement machine learning technologies
-      </h1>
-    </main>
-  )
+    return (
+        <main className="min-h-screen min-w-screen dark:text-white dark:bg-zinc-900">
+            <h1 className="pt-6 text-4xl font-bold lg:text-6xl">
+                I implement machine learning technologies
+            </h1>
+        </main>
+    )
 }
 
 export default App
