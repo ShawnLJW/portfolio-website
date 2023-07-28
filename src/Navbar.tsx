aadb84ae53bf5972912a2130a9d0e1673be95b06
Shawn Lee <thisisshawnlee@gmail.com>
2023-07-28 10:49:09 +0800

    add tailwind

---
+++ src/Navbar.tsx
@@ -0,0 +1,9 @@
+function Navbar() {
+    return (
+        <nav className="fixed w-screen bg-white dark:text-white dark:bg-zinc-900">
+            <a href="/" className="font-semibold">shawnljw</a>
+        </nav>
+    )
+}
+
+export default Navbar
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import Navbar from './Navbar.tsx'
 import './index.css'
+
+ReactDOM.createRoot(document.getElementById('navbar')!).render(
+  <React.StrictMode>
+    <Navbar />
+  </React.StrictMode>,
+)
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
